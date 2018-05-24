db59cade05a99bb67bca50f1ec8eba1a53990616
shepheam <shepheam@rose-hulman.edu>
2018-05-24 13:40:12 -0400

    DONE

--- src/problem1.py
+++ src/problem1.py
@@ -20,7 +20,7 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
     run_test_problem1a()
-    # run_test_problem1b()
+    run_test_problem1b()
 
 
 def run_test_problem1a():
@@ -172,10 +172,14 @@ def problem1b(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    total = 0
+    for k in range(int(len(numbers) / 3), int(len(numbers) / 3) * 2):
+        total = total + numbers[k]
+    return total
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
